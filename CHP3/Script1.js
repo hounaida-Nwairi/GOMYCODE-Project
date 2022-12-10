f2f953a1c34e610aa4b2b5cf16e77b58cc217b8d
NOUAIRI Hounaida <hounaidanwairi@gmail.com>
2022-12-10 10:47:55 +0100

    a change was made

---
+++ CHP3/Script1.js
@@ -0,0 +1,35 @@
+
+function show() { 
+
+
+var x = document.getElementById("btn").innerHTML;
+x = parseInt(x, 10);
+
+x += 1;
+
+document.getElementById("btn").innerHTML = x;
+
+
+}
+function reverse(){
+    var x = document.getElementById("btn").innerHTML;
+    if (x="Click me")  {
+        document.getElementById("btn").innerHTML = "me Click";
+    }
+    else {
+        document.getElementById("btn").innerHTML = "Click me";
+    }
+
+}
+function compare() {
+var x = 'a'
+var y =1
+
+if(x>y) {
+alert("hi");
+
+}
+else{
+    alert("Uncomparable")
+}
+}
