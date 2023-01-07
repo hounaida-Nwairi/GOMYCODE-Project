7f3b0ca103d9d0b65757d5a27559de1eb531efa9
NOUAIRI Hounaida <hounaidanwairi@gmail.com>
2023-01-07 10:11:15 +0100

    a change was made

---
+++ test.js
@@ -0,0 +1,16 @@
+var a= 3
+
+switch (a) {
+	case 4:
+		alert( 'Exactly!' );
+		break;
+	default:
+		alert( "I don't know such values" );
+		break;
+	case 3:
+		alert( 'Too small' );
+		break;  
+	case 5:
+		alert( 'Too large' );
+		break; 
+}
